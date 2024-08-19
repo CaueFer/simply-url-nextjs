d5e2193c3b1ba66116298f9cad7457c39665ac52
CaueFer <cauefernandescaetano@gmail.com>
2024-08-19 15:15:57 -0300

    FIX/STYLE: fix responsive style in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <main className="relative min-h-screen flex flex-col gap-10 items-center justify-center p-24 text-white max-w-7xl">
+      <main className="relative min-h-screen flex flex-col gap-10 items-center justify-center py-24 px-6 md:px-12 lg:px-24  text-white max-w-7xl">
         <div className="flex flex-col gap-4 items-center justify-center">
           <h1 className="text-5xl md:text-6xl font-bold">Simply Url</h1>
           <h1 className="text-xl md:text-2xl text-gray-300 text-center">
